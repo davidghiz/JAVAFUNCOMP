53001f9bd2b2af76cac1b5bcd170373afa6541d4
DoViNoKi <js1045774426@hotmail.com>
2014-09-28 19:46:14 -0400

    test.java

--- test.java
+++ test.java
@@ -49,5 +49,6 @@ public class test{
 >>>>>>> 494ba910069f69ef697fd38563eb9e2b084a36ad
 =======
 	System.out.println("Hello. I'm Jessi Campbell, that guy with the afro.")
+	System.out.println("Gao Yingzhen");
     }
 }
