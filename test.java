d1cc5ccc6658114a9438ed1a941ed08376392e72
Charlotte <e.jantzlee@gmail.com>
2014-09-29 01:14:49 -0300

    making changes to C1

--- test.java
+++ test.java
@@ -1,104 +1,32 @@
 /*Please enter your name in a print line USING A PUSH REQUEST*/
 public class test{
-    public static void main(String[] args){
+    public static void main(String[] a){
 	System.out.println("Welcome to JAVAFUN");
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 	System.out.println("Ryan Colaiacovo");
-=======
 	System.out.println("Jun Guo");
-=======
-
 	System.out.println("Hi, Ryan Stevens here!");
-=======
->>>>>>> f257fcf78568ce34a1293fe3755dbce8954516a1
-	System.out.println("Ryan Colaiacovo")
->>>>>>> 0e84bad0f34b5cb2e6bb879fa3877fb7f3054a58
-=======
+	System.out.println("Ryan Colaiacovo")//missed semicolon
 	System.out.println("MATT SMITH was here");
->>>>>>> e33ca8b0d9f495c941e87b7441d962196d4e5d3f
-=======
 	System.out.println("Kevin-Geoffery O'Hearon");
->>>>>>> 8aa14d7de9f8ce41435e614f423f7ab65d2aa988
-=======
 	System.out.println("Robert Tracey");
->>>>>>> ae0ba4a6162bfbdc497cdaa3e3dd5f03729dfbb0
-=======
 	System.out.println("Matt Farrell");
->>>>>>> bdf4d8cdbac70afa994c7e6faba2417758e1bdd1
-=======
 	System.out.println("Esther Fraser");
->>>>>>> b0d63e378601b5c5ba8af3577d8def04d739332b
-=======
-   System.out.println("Daniel Whitten");
-   //hello!
->>>>>>> 4b36a66be6049c3c11e78354782677f9e7b80443
-=======
+        System.out.println("Daniel Whitten");
+        //hello!
 	System.out.println("Jorge Goldschmied Chale");
->>>>>>> e12f31710e04648a0c68153a870cf18de09dd02d
-=======
 	System.out.println("Matt Patterson");
->>>>>>> ac2e583da0ae789e0eb09f85d009e5701afa55b6
-=======
 	System.out.println("Jonathan Prall");
->>>>>>> 4e4f4b6c079cb7fd3628712c7c9cb7784a6c88c9
-=======
 	System.out.println("My name is John Phillips");
->>>>>>> 28175ee0966a0e4e6ca782b124779fdff2fec3da
-=======
 	System.out.println("Rebecca Ansems");
->>>>>>> 494ba910069f69ef697fd38563eb9e2b084a36ad
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 	System.out.println("Hello. I'm Jessi Campbell, that guy with the afro.");
 	System.out.println("Devin Peck");
-=======
-	System.out.println("Hello. I'm Jessi Campbell, that guy with the afro.")
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	
 	System.out.println("Sarah Colborne")
->>>>>>> 1a235688304bffe9e821aa67e2ed29bf5d8b3188
-=======
 	System.out.println("Jonathan Rempel");
->>>>>>> d0406f3caffbb64769652c850c21351853121615
-=======
-	System.out.println("Hello. I'm Jessi Campbell, that guy with the afro.");
->>>>>>> e54c87e32b538fd1cecaec60af8eb4d4ff639c10
-=======
-	System.out.println("Hello. I'm Jessi Campbell, that guy with the afro.");
-	System.out.println("Aqeb Hamdan");
->>>>>>> a4dae38677fb4ea2f3d53c5264145e2577131960
-=======
-	
-	System.out.println("Alexander Sokolov")
->>>>>>> 2c94e0033c52d6225333af2862007dc321d42845
-=======
+
+	System.out.println("Aqeb Hamdan");	
 	
 	System.out.println("Hello my name is Fouad Kaadou");
->>>>>>> fdd16b1f8d51eacfe259a01f2620a593ad96c6bf
-=======
 	System.out.println("Gao Yingzhen");
->>>>>>> 53001f9bd2b2af76cac1b5bcd170373afa6541d4
-=======
-	
 	System.out.println("Alexander Sokolov");
->>>>>>> 787380c44769aaf4fa9f0df2bc25a85270bb4acd
     }
 }
